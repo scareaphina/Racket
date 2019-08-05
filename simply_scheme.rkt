12a7c7fda45bfc8080e76fb495ba574ae68bdbb2
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-05 15:26:30 -0400

    Update simply_scheme.rkt

--- simply_scheme.rkt
+++ simply_scheme.rkt
@@ -128,4 +128,16 @@
   
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;
   
-  
+ ;expressing complex techniques simply in scheme
+ 
+ (define (combinations size set)
+  (cond ((= size 0) '(()))
+        ((empty? set) '())
+        (else (append (prepend-every (first set)
+                                     (combinations (- size 1)
+                                                   (butfirst set)))
+                      (combinations size (butfirst set))))))
+
+(combinations 3 '(a b c d e))
+
+(combinations 2 '(john paul george ringo))
