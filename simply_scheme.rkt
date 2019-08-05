c95f684b10f7e2b6baecdac7f188753259b4b1c9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-05 11:16:51 -0400

    Create simply_scheme.rkt

---
+++ simply_scheme.rkt
@@ -0,0 +1,16 @@
+; lambda
+
+(define (add-three number)
+  (+ number 3))
+
+(define (add-three-to-each sent)
+  (every add-three sent))
+
+(add-three-to-each (1992))
+
+;use lambda to generate a procedure that does those things for us
+
+(define (add-three-to-each sent)
+  (every (lambda (number) (+ number 3)) sent))
+
+(add-three-to-each (1 9 9 2))
