b4de7ca2f812e796a6488f67813b4d4ffbe0e951
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-07 15:44:08 -0400

    Create words_sentences.rkt

---
+++ words_sentences.rkt
@@ -0,0 +1,11 @@
+; words and sentences
+
+(quote square)
+
+(quote (tomorrow never knows))
+
+(quote (things we said today))
+
+'square ; apostrophe is an abbreviation for "quote"
+
+'(old brown shoe)
