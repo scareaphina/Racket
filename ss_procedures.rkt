2bdb02d539eecee08a0e7dbe06559b093354fb2b
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-07 11:26:03 -0400

    Update ss_procedures.rkt

--- ss_procedures.rkt
+++ ss_procedures.rkt
@@ -8,3 +8,22 @@
 (+ 10 (square 2))
 
 (square (square 3))
+
+;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (square x)
+  (* x x))
+
+(square 7)
+
+(+ 10 (square 2))
+
+(square (square 3))
+
+(define (f x)
+  (+ (* 3 x) 12))
+
+(define (g x)
+  (* 3 (+ x 4)))
+
+;;;;;;;;;;;;;;;;;;;;;;;
