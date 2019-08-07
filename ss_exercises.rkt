5115569207a752fd568a5ef12a396954fa73616f
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-07 16:03:58 -0400

    Update ss_exercises.rkt

--- ss_exercises.rkt
+++ ss_exercises.rkt
@@ -63,3 +63,21 @@
 (tip 29.23)
 
 (tip 7.54)
+
+; 5.14
+
+(define (third thing)
+  (first (butfirst
+          (butfirst thing))))
+
+(third 'and)
+
+5.15
+
+(define (first-two wd)
+  (word
+   (first wd) (first (butfirst wd))))
+
+(first-two 'seraphina)
+
+(first-two 'ambulatory)
