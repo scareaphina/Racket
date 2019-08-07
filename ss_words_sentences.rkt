76568e2a42781b4e85b8be606a108b0361a06f77
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-07 15:48:39 -0400

    Update and rename words_sentences.rkt to ss_words_sentences.rkt

---
+++ ss_words_sentences.rkt
@@ -0,0 +1,60 @@
+; words and sentences
+
+(quote square)
+
+(quote (tomorrow never knows))
+
+(quote (things we said today))
+
+'square ; apostrophe is an abbreviation for "quote"
+
+'(old brown shoe)
+
+; selectors
+
+(first 'something)
+
+(first '(eight days a week))
+
+(first 910)
+
+(last 'something)
+
+(butfirst 'something)
+
+(butfirst '(eight days a week))
+
+(butfirst 910)
+
+(butlast 'something)
+
+(butlast '(eight days a week))
+
+(butlast 910)
+
+; can use primitive selectors to define others
+
+(define (second thing)
+  (first (butfirst thing)))
+
+(second '(like dreamers do))
+
+(second 'michelle)
+
+; primitive selector item that takes two arguments: a number "n" and a word or sentence
+
+(item 4 '(being for the benefit of mister kite!))
+
+(item 4 'benefit)
+
+(first 'because)
+
+(first '(because))
+
+(butfirst 'because)
+
+(butfirst '(because))
+
+(butfirst 'a)
+
+(butfirst 1024)
