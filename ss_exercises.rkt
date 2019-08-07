730e48511e3e26a4bb7fe86c2ab09dc850eaad2c
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-07 11:50:10 -0400

    Update ss_exercises.rkt

--- ss_exercises.rkt
+++ ss_exercises.rkt
@@ -13,3 +13,19 @@
 
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
+
+; 4.5
+
+; F=9⁄5C+32
+
+(define (cel-to-fahr cel)
+  (+ (* (/ 9 5) cel) 32))
+
+(cel-to-fahr 32)
+
+; C=5⁄9(F-32)
+
+(define (fahr-to-cel fahr)
+  (* (/ 5 9) (- fahr 32)))
+
+(fahr-to-cel 95)
