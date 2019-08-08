943c6482267d6d2dfc52aaae9273a530d6e17b55
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-08 10:28:07 -0400

    Update ss_booleans.rkt

--- ss_booleans.rkt
+++ ss_booleans.rkt
@@ -1,2 +1,10 @@
 #lang simply-scheme
 
+(define (greet name)
+  (if (equal? (first name) 'professor)
+      (se '(i hope i am not bothering you) 'professor (last name))
+      (se '(good to see you) (first name))))
+
+(greet '(matt wright))
+
+(greet '(professor harold abelson))
