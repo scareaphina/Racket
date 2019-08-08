1db473d265055f0f0524f310a73c9be30a0dcdc1
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-08 11:54:21 -0400

    Update ss_booleans.rkt

--- ss_booleans.rkt
+++ ss_booleans.rkt
@@ -155,3 +155,30 @@
 (define (integer-quotient big little) ; alternate version
   (and (divisible? big little)
        (/ big little)))
+
+(define (roman-value letter)
+  (if (equal? letter 'i)
+      1
+      (if (equal? letter 'v)
+          5
+          (if (equal? letter 'x)
+              10
+              (if (equal? letter 'l)
+                  50
+                  (if (equal? letter 'c)
+                      100
+                      (if (equal? letter 'd)
+                          500
+                          (if (equal? letter 'm)
+                              1000
+                              'huh?))))))))
+
+(define (roman-value letter)
+  (cond ((equal? letter 'i) 1)
+        ((equal? letter 'v) 5)
+        ((equal? letter 'x) 10)
+        ((equal? letter 'l) 50)
+        ((equal? letter 'c) 100)
+        ((equal? letter 'd) 500)
+        ((equal? letter 'm) 1000)
+        (else 'huh?)))
