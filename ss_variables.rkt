5e69940c730cc944d35c94e3a8f347f4ab3f9d30
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-09 14:17:01 -0400

    Update ss_variables.rkt

--- ss_variables.rkt
+++ ss_variables.rkt
@@ -24,3 +24,28 @@
   (+ x y))
 
 (f 4)
+
+(define (roots a b c)
+  (se (/ (+ (- b) (sqrt (- (* b b) (* 4 a c))))
+         (* 2 a))
+      (/ (- (- b) (sqrt (- (* b b) (* 4 a c))))
+         (* 2 a))))
+         
+(define (roots a b c)
+  (roots1 a b c (sqrt (- (* b b) (* 4 a c)))))
+
+(define (roots1 a b c discriminant)
+  (se (/ (+ (- b) discriminant) (* 2 a))
+      (/ (- (- b) discriminant) (* 2 a))))
+      
+(define (roots a b c)
+  (let ((discriminant (sqrt (- (* b b) (* 4 a c)))))
+    (se (/ (+ (- b) discriminant) (* 2 a))
+        (/ (- (- b) discriminant) (* 2 a)))))
+        
+(define (roots a b c)
+  (let ((discriminant (sqrt (- (* b b) (* 4 a c))))
+        (minus-b (- b))
+        (two-a (* 2 a)))
+    (se (/ (+ minus-b discriminant) two-a)
+        (/ (- minus-b discriminant) two-a))))        
