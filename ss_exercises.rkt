8f7e60fef664e6de31c693a1e94948d3e23d901e
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-12 14:36:25 -0400

    Update ss_exercises.rkt

--- ss_exercises.rkt
+++ ss_exercises.rkt
@@ -261,3 +261,12 @@
 (who '(sells out))
 
 '(pete sells out roger sells out john sells out keith sells out)
+
+; 9.5
+
+(define (prepend-every ltr wd)
+  (every (lambda (wd) (word ltr wd)) wd))
+
+(prepend-every 's '(he aid he aid))
+
+(prepend-every 'anti '(dote pasto gone body))
