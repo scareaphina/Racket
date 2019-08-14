0e55e3234c97bf90048e7a9914b3b1deb59a0e0d
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-14 09:48:47 -0400

    Update ss_exercises.rkt

--- ss_exercises.rkt
+++ ss_exercises.rkt
@@ -270,3 +270,32 @@
 (prepend-every 's '(he aid he aid))
 
 (prepend-every 'anti '(dote pasto gone body))
+
+; 11.2
+
+(define (count-ums0 sent)
+  0)
+
+(define (count-ums1 sent)
+  (if (equal? 'um (first sent))
+      1
+      0))
+
+(define (count-ums2 sent)
+  (if (equal? 'um (first sent))
+      (+ 1 (count-ums1 (bf sent)))
+      (count-ums1 (bf sent))))
+
+(define (count-ums3 sent)
+  (if (equal? 'um (first sent))
+      (+ 1 (count-ums2 (bf sent)))
+      (count-ums2 (bf sent))))
+
+(define (count-ums sent)
+  (if (< (count sent) 1)
+      0
+      (if (equal? 'um (first sent))
+          (+ 1 (count-ums (bf sent))
+             (count-ums (bf sent))))))
+
+(count-ums3 '(what if we um do um this))
