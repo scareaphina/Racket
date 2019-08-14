5dca2f06837d5e91b91fa1ffc6c60f4e883ebdbd
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-14 13:49:58 -0400

    Update ss_recursion.rkt

--- ss_recursion.rkt
+++ ss_recursion.rkt
@@ -209,3 +209,12 @@
             (reverse (bl word)))))
 
 (reverse 'unicorn)
+
+(define (downup wd)
+  (if (= (count wd) 1)
+      (se wd)
+      (se wd (downup (bl wd)) wd)))
+
+(downup 'a)
+
+(downup 'alphabet)
