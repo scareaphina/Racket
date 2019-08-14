d32ebdf8a2d94791645225580cb18b567345eabf
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-14 14:11:46 -0400

    Update ss_exercises.rkt

--- ss_exercises.rkt
+++ ss_exercises.rkt
@@ -364,3 +364,14 @@
 (countdown 15)
 
 (countdown 5)
+
+; 12.2
+
+(define (acronym sent) 
+  (if (= (count sent) 1)
+      (first (first sent))
+      (word (first (first sent))
+	    (acronym (bf sent)))))
+
+(acronym '(bananas are good))
+(acronym '(a))
