ac223aae55ba64b41975de9e1b21098b2c0badf0
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-21 11:30:41 -0400

    Update ss_recursion.rkt

--- ss_recursion.rkt
+++ ss_recursion.rkt
@@ -495,3 +495,31 @@
 
 (sent-before? '(strawberry fields forever)
               '(strawberry fields usually))
+
+; advanced recursion
+
+; sort
+
+(define (sort sent)
+  (if (empty? sent)
+      '()
+      (se (earliest-word sent)
+          (sort (remove-once (earliest-word sent) sent)))))
+
+(define (earliest-word sent)
+  (earliest-helper (first sent) (bf sent)))
+
+(define (earliest-helper so-far rest)
+  (cond ((empty? rest) so-far)
+        ((before? so-far (first rest))
+         (earliest-helper so-far (bf rest)))
+        (else (earliest-helper (first rest) (bf rest)))))
+
+(define (remove-once wd sent)
+  (cond ((empty? sent) '())
+	((equal? wd (first sent)) (bf sent))
+	(else (se (first sent) (remove-once wd (bf sent))))))
+
+(sort '(i wanna be your man))
+
+(before? 'starr 'best)
