4bc05837d5cdb819cb94ca353beda4eaa4ba5b37
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-22 09:47:31 -0400

    Update Ch14.rkt

--- SS_Exercises/Ch14.rkt
+++ SS_Exercises/Ch14.rkt
@@ -1,1 +1,13 @@
+#lang simply-scheme
 
+; 14.1
+
+(define (remove-once wd sent)
+  (cond ((empty? sent) '())
+        ((equal? (first sent) wd) (bf sent))
+        (else (se (first sent) (remove-once wd (bf sent))))))
+
+(trace remove-once)
+
+(remove-once 'morning '(good morning good morning))
+(remove-once 'oh '(oh bla di oh bla dah))
