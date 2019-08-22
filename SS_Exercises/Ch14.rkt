8ce778553b7719051d6cb3792a38662405931274
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-22 09:55:47 -0400

    Update Ch14.rkt

--- SS_Exercises/Ch14.rkt
+++ SS_Exercises/Ch14.rkt
@@ -11,3 +11,15 @@
 
 (remove-once 'morning '(good morning good morning))
 (remove-once 'oh '(oh bla di oh bla dah))
+
+; 14.2
+
+(define (up word)
+  (if (= (count word) 1)
+      word
+      (se (up (bl word)) word)))
+
+(trace up)
+
+(up 'town)
+(up 'banana)
