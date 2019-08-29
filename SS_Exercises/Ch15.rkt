19eaccd65496c52ee473d55371935dacc8ac7bca
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-29 14:54:25 -0400

    Update Ch15.rkt

--- SS_Exercises/Ch15.rkt
+++ SS_Exercises/Ch15.rkt
@@ -1,1 +1,18 @@
-; 15.1
+; 15.2
+
+(define (implode sent)
+  (accumulate word sent))
+
+(define (check-pal wd)
+  (if (<= (count wd) 1)
+      #t
+      (and (equal? (first wd) (last wd))
+           (check-pal (bf (bl wd))))))
+
+(define (palindrome? sent)
+  (check-pal (implode sent)))
+
+(trace palindrome?)
+
+(palindrome? '(flee to me remote elf))
+(palindrome? '(flee to me remote control))
