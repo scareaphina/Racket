be35dd46e6ca416c47a605f369825ea4db9f0154
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-05 15:13:40 -0400

    Update Ch17.rkt

--- SS_Exercises/Ch17.rkt
+++ SS_Exercises/Ch17.rkt
@@ -1,1 +1,21 @@
+; 17.1
 
+(car '(rod chris colin hugh paul))
+
+(cadr '(rod chris colin hugh paul))
+
+(cdr '(rod chris colin hugh paul))
+
+(car '(rod))
+
+(cons '(rod argent) '(chris white))
+
+(append '(rod argent) '(chris white))
+
+(list '(rod argent) '(chris white))
+
+(caadr '((rod argent) (chris white) (colin blunstone) (hugh grundy) (paul atkinson)))
+
+(assoc 'colin '((rod argent) (chris white) (colin blunstone) (hugh grundy) (paul atkinson)))
+
+(assoc 'argent '((rod argent) (chris white) (colin blunstone) (hugh grundy) (paul atkinson)))
