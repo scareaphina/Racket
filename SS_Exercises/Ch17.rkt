3849728627f6de4d22ec4663a9435d8cceb0d745
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-10 13:48:50 -0400

    Update Ch17.rkt

--- SS_Exercises/Ch17.rkt
+++ SS_Exercises/Ch17.rkt
@@ -19,3 +19,21 @@
 (assoc 'colin '((rod argent) (chris white) (colin blunstone) (hugh grundy) (paul atkinson)))
 
 (assoc 'argent '((rod argent) (chris white) (colin blunstone) (hugh grundy) (paul atkinson)))
+
+; 17.4
+
+(define (mystery lst)
+  (mystery-helper lst '()))
+
+(define (mystery-helper lst other)
+  (if (null? lst)
+      other
+      (mystery-helper (cdr lst) (cons (car lst) other))))
+
+(check-equal? (mystery '(1 2 3)) '(3 2 1))
+(check-equal? (mystery '(a l p h a b e t)) '(t e b a h p l a))
+
+(mystery '(1 2 3))
+(mystery '(a l p h a b e t))
+
+; this function returns the reverse of a list it is given
