de019c25f421fab5664805fb641298f884e57d96
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-18 10:38:09 -0400

    Update tictactoe.rkt

--- tictactoe.rkt
+++ tictactoe.rkt
@@ -1,1 +1,26 @@
 https://people.eecs.berkeley.edu/~bh/ssch10/ttt.html
+
+; tic tac toe
+
+; (ttt '__o_xox_x 'o)
+; Here is a sample game:
+
+; > (ttt '____x____ 'o)                   ; Human goes first in square 5
+; 1                                       ; Computer moves in square 1
+; > (ttt 'o__xx____ 'o)                   ; Human moves in square 4
+; 6                                       ; Computer blocks in square 6
+; > (ttt 'o_xxxo___ 'o)                   ; Human moves in square 3
+; 7                                       ; Computer blocks again
+; > (ttt 'o_xxxoox_ 'o)
+; 2
+
+; defining set of choices in terms of a large cond expression
+
+;(define (ttt position me)
+;  (cond ((i-can-win?)
+;         (choose-winning-move))
+;        ((opponent-can-win?)
+;         (block-opponent-win))
+;        ((i-can-win-next-time?)
+;         (prepare-win))
+;        (else (whatever))))
